4e7a7afe5f1037b3f96f8494c4f9749bd2b8866a
Dat <dnguyenvan344@gmail.com>
2022-04-27 22:36:15 +0700

    ''

--- admin/layout/addCategory.php
+++ admin/layout/addCategory.php
@@ -23,41 +23,41 @@
                     <h2>Thêm Mới Danh Mục</h2>
                 </div>
                 <div class="card-body">
-                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
                         <div class="form-group">
+                            <?php 
+                                $id = $_GET['id'];
+                                if (isset($id))
+                                    echo "<input type='hidden' name='categoryId' value=$id>";
+                            ?>
                             <label for="category">Nhập danh mục: </label>
                             <input type="text" name="category" id="category" class="form-control col-4" placeholder="Tên danh mục"
                             <?php
+                                $con = mysqli_connect('localhost', 'root', 'milkyway', 'projectphp');
                                 $id = $_GET['id'];
-                                if ($id != '' && $id != null) {
-                                    $con = mysqli_connect('localhost', 'root', 'milkyway', 'projectphp');
+                                if (isset($id)) {
                                     $sql = "select * from category where id = $id";
                                     $result = mysqli_query($con, $sql);
                                     $row = mysqli_fetch_assoc($result);
-                                    echo "value='" .$row['name'] . "'";
-                                    mysqli_close($con);
+                                    echo "value='" .$row['name']. "'";
                                 }
+                                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                                    $name = $_POST['category'];
+                                    $categoryId = $_POST['categoryId'];
+                                    if (isset($categoryId)) {
+                                        mysqli_query($con, "UPDATE category SET name='$name' WHERE id = $categoryId");
+                                    }
+                                    else{
+                                        mysqli_query($con, "INSERT INTO category(name) VALUES ('$name')");
+                                    }
+                                }
+                                mysqli_close($con);
                             ?>
                             >
                         </div>
                         <button type="submit" class="btn btn-primary">Lưu</button>
                     </form>
                 </div>
-                <?php
-                    $con = mysqli_connect('localhost', 'root', 'milkyway', 'projectphp');
-                    $name = $_POST['category'];
-                    $id = $_GET['id'];
-                    echo $id;
-                    mysqli_query($con, "UPDATE `category` SET `id`= $id, name`= $name WHERE 1");
-                    if ($name != '' && $name != null) {
-                        if ($id != '' && $id != null) {
-                        }
-                        else {
-                           // mysqli_query($con, "INSERT INTO `category`(`name`) VALUES ('$name')");
-                        }
-                    }
-                    mysqli_close($con);
-                ?>
             </div>
         </div>
     </main>
@@ -65,5 +65,4 @@
 <script src="../js/jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="../js/admin.js"></script>
 </html>
